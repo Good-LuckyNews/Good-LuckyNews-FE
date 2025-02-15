8d225ce24be092d415601f87abf7b4649e2e1402
BB545 <black1016@hanmail.net>
2025-02-15 17:34:16 +0900

    Feat: 하단탭 필요한 컴포넌트 추가

---
+++ src/screens/GoodFeed.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const GoodFeed = () => {
+  return (
+    <Text>GoodFeed</Text>
+  )
+}
+
+export default GoodFeed
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+import GoodFeed from './GoodFeed';
+import GoodNews from './GoodNews';
+import My from './My';
+
+export { Home, GoodFeed, GoodNews, My };
