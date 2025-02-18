cc4ae880bfc19eac7e8e0c525f47b1682ee3701f
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-02-18 19:54:33 +0900

    [Fix] main 브랜치 오류 임시 수정

--- src/components/My/MyCustomTopTabs.jsx
+++ src/components/My/MyCustomTopTabs.jsx
@@ -1,69 +1,70 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native';
-import { COLORS } from '../../theme/color';
-import Scrap from './Scrap';
-import MyNews from './MyNews';
-import MyCommentedNews from './MyCommentedNews';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+import { COLORS } from "../../theme/color";
 
 const shadowStyle = StyleSheet.create({
-    shadow: {
-        shadowColor: 'rgba(0, 0, 0)',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 4, // 그림자를 넣긴했지만 잘 보이진 않는 것 같음
-    },
+  shadow: {
+    shadowColor: "rgba(0, 0, 0)",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 4, // 그림자를 넣긴했지만 잘 보이진 않는 것 같음
+  },
 });
 
 const Container = styled.View`
-    flex-direction: row;
-    background-color: ${COLORS.White};
+  flex-direction: row;
+  background-color: ${COLORS.White};
 `;
 
 const CustomTabButton = styled.Pressable`
-    flex: 1;
-    height: 60px;
-    justify-content: center;
-    align-items: center;
-    border-bottom-width: 1px;
-    border-bottom-color: ${({ selected }) => (selected ? COLORS.MainYellow : COLORS.Gray)};
+  flex: 1;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ selected }) =>
+    selected ? COLORS.MainYellow : COLORS.Gray};
 `;
 
 const tabs = ["스크랩", "작성한 소식", "답글 단 소식"];
 
 const MyCustomTopTabs = () => {
-    const [selectedTab, setSelectedTab] = useState(0);
+  const [selectedTab, setSelectedTab] = useState(0);
 
-    const renderComponent = () => {
-        // switch (selectedTab) {
-        //     case 0: return <Scrap />;
-        //     case 1: return <MyNews />;
-        //     case 2: return <MyCommentedNews />;
-        //     default: return null;
-        // }
-    }
+  const renderComponent = () => {
+    // switch (selectedTab) {
+    //     case 0: return <Scrap />;
+    //     case 1: return <MyNews />;
+    //     case 2: return <MyCommentedNews />;
+    //     default: return null;
+    // }
+  };
 
-    return (
-        <View style={{ flex: 1 }}>
-            <Container style={shadowStyle.shadow}>
-                {tabs.map((tab, index) => (
-                    <CustomTabButton
-                        key={index}
-                        onPress={() => setSelectedTab(index)}
-                        selected={selectedTab === index}
-                    >
-                        <Text style={{ color: selectedTab === index ? COLORS.MainYellow : COLORS.Gray, fontWeight: 400 }}>
-                            {tab}
-                        </Text>
-                    </CustomTabButton>
-                ))}
-            </Container>
-            <View style={{ flex: 1 }}>
-                {renderComponent()}
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={{ flex: 1 }}>
+      <Container style={shadowStyle.shadow}>
+        {tabs.map((tab, index) => (
+          <CustomTabButton
+            key={index}
+            onPress={() => setSelectedTab(index)}
+            selected={selectedTab === index}
+          >
+            <Text
+              style={{
+                color: selectedTab === index ? COLORS.MainYellow : COLORS.Gray,
+                fontWeight: 400,
+              }}
+            >
+              {tab}
+            </Text>
+          </CustomTabButton>
+        ))}
+      </Container>
+      <View style={{ flex: 1 }}>{renderComponent()}</View>
+    </View>
+  );
+};
 
-export default MyCustomTopTabs
+export default MyCustomTopTabs;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,7 @@
-import Home from './Home';
-import GoodFeed from './GoodFeed';
-import GoodNews from './GoodNews';
-import My from './My';
+import Home from "./Home";
+import GoodFeed from "./GoodFeed";
+import GoodNews from "./GoodNews";
+import My from "./My";
+import Home2 from "./Home2";
 
-export { Home, GoodFeed, GoodNews, My };
+export { Home, GoodFeed, GoodNews, My, Home2 };
