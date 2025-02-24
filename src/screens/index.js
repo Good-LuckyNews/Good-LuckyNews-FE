cfd71c68470d3fb3cf069ac935f56373e4a22692
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-02-24 21:39:04 +0900

    Feat: 플레이스 만들기 페이지 구현
    플레이스 만들기 버튼을 눌렀을 때의 기능과 이미지 불러오기 기능은 아직 미구현

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,8 @@
-import Home from './Home';
-import GoodFeed from './GoodFeed';
-import GoodNews from './GoodNews';
-import My from './My';
+import Home from "./Home";
+import GoodFeed from "./GoodFeed";
+import GoodNews from "./GoodNews/GoodNews";
+import My from "./My";
+import Home2 from "./Home2";
+import MakePlace from "./GoodNews/MakePlace";
 
-export { Home, GoodFeed, GoodNews, My };
+export { Home, GoodFeed, GoodNews, My, Home2, MakePlace };
