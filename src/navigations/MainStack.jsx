c590307489910e5db87ac476ee49a1dbf2a25810
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-02-25 18:47:45 +0900

    Fix: 로그인/회원가입 스택 정비
    각 창을 직접 불러오는게 아니라 LoginStack 을 이용해서 불러오게 수정함.

--- src/navigations/MainStack.jsx
+++ src/navigations/MainStack.jsx
@@ -1,37 +1,18 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import MainTab from "./MainTab";
-import { Login, SignUp } from "../screens";
-import { COLORS } from "../theme/color";
+import LoginStack from "./Login/LoginStack";
 
 const Stack = createStackNavigator();
 
 const MainStack = () => {
-  const signUpScreenOption = {
-    headerShown: true,
-    headerTitleAlign: "center",
-    headerTintColor: COLORS.Black,
-    cardStyle: { backgroundColor: COLORS.White },
-    headerBackTitle: "",
-    headerTitle: "회원가입",
-    headerLeftContainerStyle: { paddingLeft: 10 },
-    headerRightContainerStyle: { paddingRight: 26 },
-  };
-
   return (
     <Stack.Navigator
       initialRouteName="Main"
-      screenOptions={{
-        headerShown: false,
-      }}
+      screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen
-        name="SignUp"
-        component={SignUp}
-        options={signUpScreenOption}
-      />
       <Stack.Screen name="Main" component={MainTab} />
+      <Stack.Screen name="LoginStack" component={LoginStack} />
     </Stack.Navigator>
   );
 };
