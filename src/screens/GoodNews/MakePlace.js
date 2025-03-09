c2a19b5976d645dcb284c760ebacb4c68154bcc8
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-03-09 18:07:11 +0900

    Feat: 플레이스 만들기 axios 연동

--- src/screens/GoodNews/MakePlace.js
+++ src/screens/GoodNews/MakePlace.js
@@ -8,64 +8,170 @@ import {
   View,
 } from "react-native";
 import { COLORS } from "../../theme/color";
-import { NextStepButton } from "../../components";
+import { CustomAlert, NextStepButton } from "../../components";
+import * as SecureStore from "expo-secure-store";
+import * as ImagePicker from "expo-image-picker";
+import api from "../../utils/common";
+import { useNavigation } from "@react-navigation/native";
 
 const MakePlace = () => {
   const [placeName, setPlaceName] = useState("");
   const [placeIntro, setPlaceIntro] = useState("");
+  const [imageSrc, setImageSrc] = useState(null);
+  const [alert, setAlert] = useState("");
+  const [permissionStatus, setPermissionStatus] = useState(null);
+  const navigation = useNavigation();
+
+  // 권한 요청 함수
+  const requestPermission = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    setPermissionStatus(status);
+  };
+
+  // 이미지 선택 함수
+  const pickImage = async () => {
+    if (permissionStatus === null) {
+      await requestPermission(); // 권한 요청
+    }
+
+    if (permissionStatus === "granted") {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      });
+      console.log(result);
+      if (!result.canceled) {
+        const imageUri = result.assets[0].uri;
+        setImageSrc(imageUri); // 이미지 URI 상태에 저장
+        console.log("Image URI:", imageUri);
+      } else {
+        console.log("failure");
+      }
+    } else {
+      alert("권한이 필요합니다!"); // 권한이 없는 경우
+    }
+  };
+
+  const makePlace = async () => {
+    // 조건 검사
+    if (!imageSrc || !placeName || !placeIntro) {
+      setAlert("필수 입력 항목을 모두 작성해주세요!");
+      return;
+    } else if (placeIntro.length > 200) {
+      setAlert("플레이스 소개는 200자 이내로 작성해주세요.");
+      return;
+    }
+
+    try {
+      // 토큰 조회
+      const token = await SecureStore.getItemAsync("userToken");
+      if (!token) {
+        console.log("No token found");
+        return;
+      }
+
+      // axios 연동
+      async function makePlaceAxios() {
+        try {
+          const formData = new FormData();
+          formData.append("image", { imageSrc });
+          formData.append("placeName", placeName);
+          formData.append("placeDetails", placeIntro);
+
+          const response = await api.post(`/api/place`, formData, {
+            headers: { Authorization: token },
+          });
+          navigation.goBack();
+        } catch (error) {
+          if (error.response) {
+            // 서버 응답이 있는 경우
+            console.error("Response error:", error.response);
+            console.log("\n");
+            console.error("Status code:", error.response.status);
+            console.log("\n");
+            console.error("Error data:", error.response.data);
+          } else if (error.request) {
+            // 요청은 보내졌으나 응답을 받지 못한 경우
+            console.error("Request error:", error.request);
+          } else {
+            // 에러 메시지
+            console.error("Error message:", error);
+          }
+          setAlert("플레이스 만들기에 실패하였습니다.");
+        }
+      }
+
+      makePlaceAxios();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.mainTitle}>플레이스 만들기</Text>
+    <>
+      <CustomAlert
+        message={alert}
+        backgroundColor={COLORS.MainYellow}
+        visible={!!alert}
+        onHide={() => setAlert("")}
+      />
+      <View style={styles.container}>
+        <Text style={styles.mainTitle}>플레이스 만들기</Text>
 
-      <View style={{ paddingHorizontal: 12 }}>
-        <View style={styles.uploadImageContainer}>
-          <Image
-            source={require("../../../assets/images/uploadImage/default_place_image.png")}
-            style={styles.imagePreview}
-          />
-          <Pressable onPress={() => console.log("upload image")}>
+        <View style={{ paddingHorizontal: 12 }}>
+          <Pressable style={styles.uploadImageContainer} onPress={pickImage}>
+            <Image
+              source={
+                !!imageSrc
+                  ? { uri: imageSrc }
+                  : require("../../../assets/images/uploadImage/default_place_image.png")
+              }
+              style={styles.imagePreview}
+            />
             <Image
               source={require("../../../assets/images/uploadImage/upload_image_button.png")}
               style={styles.uploadImageButton}
             />
           </Pressable>
-        </View>
-        <RequiredText style={{ alignSelf: "center", marginBottom: 43 }} />
+          <RequiredText style={{ alignSelf: "center", marginBottom: 43 }} />
 
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>플레이스명</Text>
-          <RequiredText />
-        </View>
-        <TextInput
-          placeholder="플레이스명을 작성해주세요."
-          value={placeName}
-          onChangeText={setPlaceName}
-          style={[styles.textInputCommon, styles.textInputSmall]}
-          placeholderTextColor="#ADADAD"
-        />
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>플레이스명</Text>
+            <RequiredText />
+          </View>
+          <TextInput
+            placeholder="플레이스명을 작성해주세요."
+            value={placeName}
+            onChangeText={setPlaceName}
+            style={[styles.textInputCommon, styles.textInputSmall]}
+            placeholderTextColor="#ADADAD"
+          />
 
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>플레이스 소개</Text>
-          <RequiredText />
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>플레이스 소개</Text>
+            <RequiredText />
+          </View>
+          <TextInput
+            placeholder="간략한 소개를 200자 이내로 작성해주세요."
+            multiline
+            numberOfLines={3}
+            value={placeIntro}
+            onChangeText={setPlaceIntro}
+            style={[styles.textInputCommon, styles.textInputLarge]}
+            placeholderTextColor="#ADADAD"
+          />
         </View>
-        <TextInput
-          placeholder="간략한 소개를 200자 이내로 작성해주세요."
-          multiline
-          numberOfLines={3}
-          value={placeIntro}
-          onChangeText={setPlaceIntro}
-          style={[styles.textInputCommon, styles.textInputLarge]}
-          placeholderTextColor="#ADADAD"
+
+        <NextStepButton
+          width={339}
+          text="플레이스 만들기"
+          style={{ position: "absolute", bottom: 113, marginHorizontal: "27" }}
+          onPress={makePlace}
         />
       </View>
-
-      <NextStepButton
-        width={339}
-        text="플레이스 만들기"
-        style={{ position: "absolute", bottom: 113, marginHorizontal: "27" }}
-      />
-    </View>
+    </>
   );
 };
 
@@ -115,6 +221,7 @@ const styles = StyleSheet.create({
   imagePreview: {
     width: 91,
     height: 91,
+    borderRadius: "100%",
   },
   uploadImageButton: {
     width: 19,
