68b0918354e8e4a68d64fe7b51ef33c08b8b2e1e
BB545 <black1016@hanmail.net>
2025-07-17 02:28:27 +0900

    [feat/#36] API Base_URL 변경

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://draconist.goodluckynews.store", // 실제 API 주소 입력
+  baseURL: "https://goodluckynews.store", // 실제 API 주소 입력
   timeout: 10000, // 10초 타임아웃 설정
 });
 
